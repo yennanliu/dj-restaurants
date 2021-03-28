044b1e98dd3f648d0ae31cfea193fe3d7157132b
yennanliu <f339339@gmail.com>
2021-03-28 15:56:57 +0800

    update

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django import template
-from django.template.loader import get_template
+#from django.template.loader import get_template
+from django.shortcuts import render_to_response
 
 def here(request):
     return HttpResponse("helloooo! i am here !!")
@@ -12,9 +13,13 @@ def add(request, a, b):
     d = a - b
     p = a * b
     q = a / b
-    t = get_template('math.html')
+    return render_to_response(
+        'math.html',
+        {'s': s, 'd' : d, 'p': p, 'q': q}
+    )
+    #t = get_template('math.html')
     #t = template.Template('<html>sum= {{s}}<br>dif = {{d}}<br>pro={{p}} <br> quo = {{q}} </html>')
-    c = template.Context({'s': s, 'd' : d, 'p': p, 'q': q})
-    return HttpResponse(t.render(c))
+    #c = template.Context({'s': s, 'd' : d, 'p': p, 'q': q})
+    #return HttpResponse(t.render(c))
     # s = int(a) + int(b)
     # return HttpResponse(str(s))
