20127170f4ff1c18a5bb8b908f71df10f849819b
yennanliu <f339339@gmail.com>
2021-03-28 15:45:00 +0800

    updaye view

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
 
+from django import template
+
 def here(request):
     return HttpResponse("helloooo! i am here !!")
 
 def add(request, a, b):
-    s = int(a) + int(b)
-    return HttpResponse(str(s))
+    a = int(a)
+    b = int(b)
+    s = a + b 
+    d = a - b
+    p = a * b
+    q = a / b
+    t = template.Template('<html>sum= {{s}}<br>dif = {{d}}<br>pro={{p}} <br> quo = {{q}} </html>')
+    c = template.Context({'s': s, 'd' : d, 'p': p, 'q': q})
+    return HttpResponse(t.render(c))
+    # s = int(a) + int(b)
+    # return HttpResponse(str(s))
