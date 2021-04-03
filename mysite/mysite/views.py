79f9e17eb1b60c737657f58057deba53ec457d76
yennanliu <f339339@gmail.com>
2021-04-03 16:48:31 +0800

    fix/update mysite/mysite/views.py, update migrations results

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,6 +3,8 @@ from django import template
 #from django.template.loader import get_template
 from django.shortcuts import render_to_response
 
+from restaurants.models import Restaurant, Food
+
 def here(request):
     return HttpResponse("helloooo! i am here !!")
 
@@ -24,26 +26,28 @@ def add(request, a, b):
     # s = int(a) + int(b)
     # return HttpResponse(str(s))
 
+# def menu(request):
+#     food1 = {'name': 'hamburger',
+#             'price' : 199,
+#             'comment' : 'great!',
+#             'is_spicy' : False
+#     }
+#     food2 = {'name': 'fries',
+#             'price' : 30,
+#             'comment' : 'yummy!',
+#             'is_spicy' : True
+#     }
+#     food3 = {'name': 'steak',
+#             'price' : 500,
+#             'comment' : 'awesome !!!!',
+#             'is_spicy' : False
+#     }
+#     # django will pass foods to menu.html, and for loop all food via jinja syntax
+#     foods = [food1, food2, food3]  
+#     # locals() will return all local vars in this method
+#     return render_to_response('menu.html', locals())
+
+
 def menu(request):
-    food1 = {'name': 'hamburger',
-            'price' : 199,
-            'comment' : 'great!',
-            'is_spicy' : False
-    }
-    food2 = {'name': 'fries',
-            'price' : 30,
-            'comment' : 'yummy!',
-            'is_spicy' : True
-    }
-    food3 = {'name': 'steak',
-            'price' : 500,
-            'comment' : 'awesome !!!!',
-            'is_spicy' : False
-    }
-    # django will pass foods to menu.html, and for loop all food via jinja syntax
-    foods = [food1, food2, food3]  
-    # locals() will return all local vars in this method
-    return render_to_response('menu.html', locals())
-
-
-
+    restaurants = Restaurants.objects.all()
+    return render_to_response("menu.html", locals())
