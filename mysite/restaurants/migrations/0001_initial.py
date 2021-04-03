801e52a9267d103ea59b56ef0dcc00cb7c5ff667
yennanliu <f339339@gmail.com>
2021-04-03 17:41:02 +0800

    fix /templates/menu.html  syntax, update progress.md, gitignore, migrations/0001_initial.py

--- mysite/restaurants/migrations/0001_initial.py
+++ mysite/restaurants/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Food',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('name', models.CharField(max_length=20)),
                 ('price', models.DecimalField(max_digits=3, decimal_places=0)),
                 ('comment', models.CharField(max_length=50, blank=True)),
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Restaurant',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('name', models.CharField(max_length=20)),
                 ('phone_number', models.CharField(max_length=15)),
                 ('address', models.CharField(max_length=50, blank=True)),
