c7fe275f2f6786291d28956a4f4b3429aec32e91
yennanliu <f339339@gmail.com>
2021-04-04 13:10:48 +0800

    add restaurants/admin.py

--- mysite/restaurants/admin.py
+++ mysite/restaurants/admin.py
@@ -3,5 +3,12 @@ from restaurants.models import Restaurant, Food
 
 # Register your models here.
 
-admin.site.register(Restaurant)
-admin.site.register(Food)
+class RestaurantAdmin(admin.ModelAdmin):
+	list_display = ('name', 'phone_number', 'address')
+
+class FoodAdmin(admin.ModelAdmin):
+	list_display = ('name', 'restaurant', 'price')
+	list_filter = ('is_spicy',)
+
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Food, FoodAdmin)
