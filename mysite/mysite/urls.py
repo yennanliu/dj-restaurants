97f3307d66eb6f17cba66f1d5634d16e2e63e701
yennanliu <f339339@gmail.com>
2021-05-15 16:57:08 +0800

    update urlpatterns (urls.py), update progress

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,18 +6,24 @@ from mysite.views import here, add, welcome, login, index, logout, register
 from restaurants.views import menu, list_restaurants, comment
 
 #from django.contrib.auth import views as auth_views
-
 # below is django default login logout method (default login, logout uses login.html under mysite/templates/registration)
 #from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns(
     '',
-    url(r'^admin/', include(admin.site.urls)),
     # mysite
-    url(r'^here/$', here),
+    url(r'^here/$', here)
+)
+
+urlpatterns += patterns(
+    # admin
+    url(r'^admin/', include(admin.site.urls))
+    )
+
+urlpatterns += patterns(
     # restaurants
     url(r'^(\d{1,2})/plus/(\d{1,2})/$', add),
-     url(r'^menu/$', menu),
+    url(r'^menu/$', menu),
     url(r'^welcome/$', welcome),
     url(r'^restaurants_list/$', list_restaurants),
     url(r'^comment/(\d{1,5})/$', comment),
@@ -25,5 +31,5 @@ urlpatterns = patterns(
     url(r'^accounts/login/$', login),
     url(r'^accounts/logout/$', logout),
     url(r'^index/$', index),
-     url(r'^accounts/register/$', register)
-)
+    url(r'^accounts/register/$', register)
+    )
