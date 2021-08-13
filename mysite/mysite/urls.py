13cfc3f6fd0e460999885a242cece89da2fada50
yennanliu <f339339@gmail.com>
2021-08-13 16:34:34 +0800

    fix syntax in mysite/mysite/urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,12 +17,12 @@ urlpatterns = [
     # mysite
     #url(r'^here/$', here),
     url(r'^here/$', HereView.as_view()),
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^admin/', admin.site.urls)
 ]
 
 urlpatterns += [
     # restaurants
-    url(r'^(\d{1,2})/plus/(\d{1,2})/$', add),
+    #url(r'^(\d{1,2})/plus/(\d{1,2})/$', add),
     #url(r'^menu/$', menu),
     url(r'^menu/(?P<pk>\d+)/$', MenuView.as_view()),
     url(r'^welcome/$', welcome),
